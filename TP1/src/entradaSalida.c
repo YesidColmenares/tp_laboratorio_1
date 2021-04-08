5721736b68906dd67500598831b7905f9d97aca1
Yesid Colmenares <rusbelt7@hotmail.com>
2021-04-08 07:22:58 -0300

    Avance Trabajo 08-04

--- TP1/src/entradaSalida.c
+++ TP1/src/entradaSalida.c
@@ -1,6 +1,62 @@
 #include "entradaSalida.h"
 
+double pedirDecimal(char cadenaTexto[], char cadenaError[], float numeroActualIngresado)
+{
 
+	double numeroDoubleIngresado;
+	int comprobacion;
+
+	numeroDoubleIngresado = 0;
+
+	printf(cadenaTexto, numeroActualIngresado);
+	fflush(stdin);
+	comprobacion = scanf("%lf", &numeroDoubleIngresado);
+	while (comprobacion == 0)
+	{
+		printf(cadenaError, numeroActualIngresado);
+		fflush(stdin);
+		comprobacion = scanf("%lf", &numeroDoubleIngresado);
+	}
+	return numeroDoubleIngresado;
+}
+
+int pedirEntero(char cadenaTexto[], char cadenaError[])
+{
+
+	int numeroIntIngresado;
+	int comprobacion;
+
+	numeroIntIngresado = 0;
+
+	printf(cadenaTexto, numeroIntIngresado);
+	fflush(stdin);
+	comprobacion = scanf("%d", &numeroIntIngresado);
+	while (comprobacion == 0)
+	{
+		printf("%s", cadenaError);
+		fflush(stdin);
+		comprobacion = scanf("%d", &numeroIntIngresado);
+	}
+	return numeroIntIngresado;
+}
+
+char pedirCaracter(char cadenaTexto[], char cadenaError[])
+{
+
+	char caracterCharIngresado;
+	int comprobacion;
+
+	printf("%s", cadenaTexto);
+	fflush(stdin);
+	comprobacion = scanf("%c", &caracterCharIngresado);
+	while (comprobacion == 0 || (caracterCharIngresado != 'A' && caracterCharIngresado != 'B' && caracterCharIngresado != 'C' && caracterCharIngresado != 'D' && caracterCharIngresado != 'E'))
+	{
+		printf("%s", cadenaError);
+		fflush(stdin);
+		comprobacion = scanf("%c", &caracterCharIngresado);
+	}
+	return caracterCharIngresado;
+}
 
 double sumar(double primerValor, double segundoValor)
 {
@@ -40,6 +96,37 @@ double dividir(double primerValor, double segundoValor)
 	resultadoDivision = primerValor / segundoValor;
 
 	return resultadoDivision;
+}
+
+double factorial(double valor)
+{
+
+	int i;
+	double resultadoFactor;
+
+	resultadoFactor = 1;
+
+	if (valor > 0)
+	{
+		for (i = (int) valor; i > 1; i--)
+		{
+			resultadoFactor *= i;
+		}
+	}
+	else
+	{
+		if (valor < 0)
+		{
+			resultadoFactor = -1;
+			for (i = (int) valor; i < 0; i++)
+			{
+				resultadoFactor *= (i * -1);
+			}
+
+		}
+	}
+
+	return resultadoFactor;
 }
 
 
--- TP1/src/main.c
+++ TP1/src/main.c
@@ -1,28 +1,11 @@
 /*
-Roossbelt Yesid Colmenares Moreno TP1
-Hacer una calculadora. Para ello el programa iniciará y contará con un menú de opciones:
-1. Ingresar 1er operando (A=x)
-2. Ingresar 2do operando (B=y)
-3. Calcular todas las operaciones
-   a) Calcular la suma (A+B)
-   b) Calcular la resta (A-B)
-   c) Calcular la division (A/B)
-   d) Calcular la multiplicacion (A*B)
-   e) Calcular el factorial (A!)
-4. Informar resultados
-   a) “El resultado de A+B es: r”
-   b) “El resultado de A-B es: r”
-   c) “El resultado de A/B es: r” o “No es posible dividir por cero”
-   d) “El resultado de A*B es: r”
-   e) “El factorial de A es: r1 y El factorial de B es: r2”
-5. Salir
-• Todas las funciones matemáticas del menú se deberán realizar en una biblioteca aparte,
-que contenga las funciones para realizar las cinco operaciones. 
-• En el menú deberán aparecer los valores actuales cargados en los operandos A y B 
-(donde dice “x” e “y” en el ejemplo, se debe mostrar el número cargado)
-• Deberán contemplarse los casos de error (división por cero, etc)
-• Documentar todas las funciones
-
+ ============================================================================
+ Name        : TP1.c
+ Author      : Yesid Colmenares
+ Version     : 1.0
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
  */
 
 #include <stdio.h>
@@ -37,11 +20,26 @@ int main(void)
 	double segundoOperandoIngresado;
 	int opcionIngresadaMenu;
 
+	double resultadoSuma;
+	double resultadoResta;
+	double resultadoDivision;
+	double resultadoMultiplicacion;
+	double resultadoPrimerFactorial;
+	double resultadoSegundoFactorial;
+
+	resultadoSuma = 0;
+	resultadoResta = 0;
+	resultadoDivision = 0;
+	resultadoMultiplicacion = 0;
+	primerOperandoIngresado = 0;
+	segundoOperandoIngresado = 0;
+	resultadoPrimerFactorial = 0;
+	resultadoSegundoFactorial = 0;
 
 	do
 	{
 		printf("\n****************************************************************");
-		printf("\n                       Bienvenido Al Sistema                    ");
+		printf("\n                      Bienvenido Al Sistema                     ");
 		printf("\n****************************************************************");
 		printf("\n1. Ingresar 1er operando (A = %.2lf)", primerOperandoIngresado);
 		printf("\n2. Ingresar 2do operando (B = %.2lf)", segundoOperandoIngresado);
@@ -54,15 +52,98 @@ int main(void)
 		switch (opcionIngresadaMenu)
 		{
 			case 1:
-				
+				primerOperandoIngresado = pedirDecimal("\n1. Ingresar 1er operando (A = %.2f): ", "1. Error, Ingresar 1er operando (A = %.2f): ", primerOperandoIngresado);
+				printf("----------------------------------------------------------------\n");
+				resultadoSuma = 0;
+				resultadoResta = 0;
+				resultadoDivision = 0;
+				resultadoMultiplicacion = 0;
+				resultadoPrimerFactorial = 0;
+				resultadoSegundoFactorial = 0;
+				break;
 
 			case 2:
-				
+				segundoOperandoIngresado = pedirDecimal("\n2. Ingresar 2do operando (B = %.2f): ", "2. Error, Ingresar 2do operando (B = %.2f): ", segundoOperandoIngresado);
+				printf("----------------------------------------------------------------\n");
+				resultadoSuma = 0;
+				resultadoResta = 0;
+				resultadoDivision = 0;
+				resultadoMultiplicacion = 0;
+				resultadoPrimerFactorial = 0;
+				resultadoSegundoFactorial = 0;
+				break;
 
 			case 3:
-				
+				printf("\n3. Calcular todas las operaciones");
+				printf("\n    A. Calcular la suma (%.2f + %.2f)", primerOperandoIngresado, segundoOperandoIngresado);
+				printf("\n    B. Calcular la resta (%.2f - %.2f)", primerOperandoIngresado, segundoOperandoIngresado);
+				printf("\n    C. Calcular la division (%.2f / %.2f)", primerOperandoIngresado, segundoOperandoIngresado);
+
+				//Validando si es posible dividir
+				if (primerOperandoIngresado != 0 && segundoOperandoIngresado != 0)
+				{
+					printf("\n    C. Calcular la division (%.2f / %.2f)", primerOperandoIngresado, segundoOperandoIngresado);
+					resultadoDivision = dividir(primerOperandoIngresado, segundoOperandoIngresado);
+				}
+
+				printf("\n    D. Calcular la multiplicacion (%.2f * %.2f)", primerOperandoIngresado, segundoOperandoIngresado);
+				printf("\n    E. Calcular el factorial (%.0f!) y (%.0f!)", primerOperandoIngresado, segundoOperandoIngresado);
+
+				//Realizamos las operaciones
+				resultadoSuma = sumar(primerOperandoIngresado, segundoOperandoIngresado);
+				resultadoResta = restar(primerOperandoIngresado, segundoOperandoIngresado);
+				resultadoMultiplicacion = multiplicar(primerOperandoIngresado, segundoOperandoIngresado);
+
+				//validando para realizar la factorizacion
+				if (primerOperandoIngresado < 171)
+				{
+					resultadoPrimerFactorial = factorial(primerOperandoIngresado);
+				}
+				if (segundoOperandoIngresado < 171)
+				{
+					resultadoSegundoFactorial = factorial(segundoOperandoIngresado);
+				}
+				printf("\n----------------------------------------------------------------\n");
+				break;
+
 			case 4:
-				
+				printf("\n4. Informar resultados");
+				printf("\n    A. El resultado de (%.2f + %.2f) es: %.2f", primerOperandoIngresado, segundoOperandoIngresado, resultadoSuma);
+				printf("\n    B. El resultado de (%.2f - %.2f) es: %.2f", primerOperandoIngresado, segundoOperandoIngresado, resultadoResta);
+
+				//validando para saber cual enunciado mostrarle al usuario
+				if (resultadoDivision != 0)
+				{
+					printf("\n    C. El resultado de (%.2f / %.2f) es: %.2f", primerOperandoIngresado, segundoOperandoIngresado, resultadoDivision);
+				}
+				else
+				{
+					printf("\n    C. !! No es posible dividir por cero !!");
+				}
+
+				printf("\n    D. El resultado de (%.2f * %.2f) es: %.2f", primerOperandoIngresado, segundoOperandoIngresado, resultadoMultiplicacion);
+
+				//validando para saber cual enunciado mostrarle al usuario
+				if (primerOperandoIngresado > 170)
+				{
+					printf("\n    E. No es posible realizar el factorial de: (%.2f!)", primerOperandoIngresado);
+				}
+				else
+				{
+					printf("\n    E. El factorial de (%d!) es: %e", (int) primerOperandoIngresado, resultadoPrimerFactorial);
+				}
+
+				if (segundoOperandoIngresado > 170)
+				{
+					printf("\n       No es posible realizar el factorial de: (%.2f!)", segundoOperandoIngresado);
+				}
+				else
+				{
+					printf("\n       y el factorial de (%d!) es: %e", (int) segundoOperandoIngresado, resultadoSegundoFactorial);
+				}
+
+				printf("\n----------------------------------------------------------------\n");
+				break;
 
 			case 5:
 				printf("\n****************************************************************");
